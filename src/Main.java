eeed9a745b3f21efdba4fe28e372b06cd1c2737d
Alexia-w-creator <afalaleeva64@gmail.com>
2024-04-18 14:53:40 +0300

    second update

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,68 @@ public class Main {
         return Integer.parseInt(number, base);
     }
 
+    static String conversionFrom10_castom(int number, int base){
+        String resNumber = "";
+        String curNumber = "";
+        while (number > 0) {
+            curNumber += (number % base);
+            if(base == 16){
+                switch (curNumber) {
+                    case ("10") -> curNumber = "A";
+                    case ("11") -> curNumber = "B";
+                    case ("12") -> curNumber = "C";
+                    case ("13") -> curNumber = "D";
+                    case ("14") -> curNumber = "E";
+                    case ("15") -> curNumber = "F";
+                    default -> {}
+                }
+            }
+            resNumber = curNumber + resNumber;
+            number = number / base;
+            curNumber = "";
+        }
+        return resNumber;
+    }
+
+    static int conversionTo10_castom(String number, int base) {
+        int resNumber = 0;
+        int curNumber = 0;
+        int n = 0;
+        for (int i = number.length() - 1; i >= 0; i--) {
+            if(base == 16){
+                switch (number.charAt(i)) {
+                    case ('0') -> curNumber = 0;
+                    case ('1') -> curNumber = 1;
+                    case ('2') -> curNumber = 2;
+                    case ('3') -> curNumber = 3;
+                    case ('4') -> curNumber = 4;
+                    case ('5') -> curNumber = 5;
+                    case ('6') -> curNumber = 6;
+                    case ('7') -> curNumber = 7;
+                    case ('8') -> curNumber = 8;
+                    case ('9') -> curNumber = 9;
+                    case ('A') -> curNumber = 10;
+                    case ('B') -> curNumber = 11;
+                    case ('C') -> curNumber = 12;
+                    case ('D') -> curNumber = 13;
+                    case ('E') -> curNumber = 14;
+                    case ('F') -> curNumber = 15;
+                    default -> {}
+                }
+            }
+            if(base == 2){
+                switch (number.charAt(i)) {
+                    case ('0') -> curNumber = 0;
+                    case ('1') -> curNumber = 1;
+                    default -> {}
+                }
+            }
+            resNumber += curNumber * Math.pow(base, n);
+            n++;
+        }
+        return resNumber;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int number = 0;
@@ -16,7 +78,7 @@ public class Main {
         String convFrom10_result = "";
         int convTo10_result = 0;
         while (true) {
-            System.out.print("Input a number: ");
+            System.out.print("\nInput a number: ");
             number = in.nextInt();
             if(number < 0 || number > 1000){
                 System.out.println("You can input only a number in the range from 0 to 1000. Try again.");
@@ -33,8 +95,17 @@ public class Main {
             convFrom10_result = conversionFrom10_Java(number, base);
             convTo10_result = conversionTo10_Java(convFrom10_result, base);
 
+            System.out.println("Built-in methods.");
             System.out.println("10 to " + base + ": " + convFrom10_result);
             System.out.println(base + " to 10" + ": " + convTo10_result);
+
+            convFrom10_result = conversionFrom10_castom(number, base);
+            convTo10_result = conversionTo10_castom(convFrom10_result, base);
+
+            System.out.println("\nOwn methods.");
+            System.out.println("10 to " + base + ": " + convFrom10_result);
+            System.out.println(base + " to 10" + ": " + convTo10_result);
+
 
             base = 0;
         }
